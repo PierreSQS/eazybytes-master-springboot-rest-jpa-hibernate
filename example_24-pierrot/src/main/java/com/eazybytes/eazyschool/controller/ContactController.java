1137aa44d7674251f53f27628ac23aa6724adc57
PierreSQS <nomo_messina@web.de>
2022-05-31 11:56:27 +0200

    Sec9_Chap98-Adding Bean Validation: initial commit

---
+++ example_24-pierrot/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -0,0 +1,36 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Controller
+public class ContactController {
+
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping("/contact")
+    public String displayContactPage() {
+        return "contact.html";
+    }
+
+    @PostMapping("/saveMsg")
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
+        return new ModelAndView("redirect:/contact");
+    }
+
+
+
+}
