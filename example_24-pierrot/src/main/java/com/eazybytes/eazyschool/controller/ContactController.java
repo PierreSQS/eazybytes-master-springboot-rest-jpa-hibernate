457fdcdd3448f80453f5a5c6c9eca2830896d32f
PierreSQS <nomo_messina@web.de>
2022-05-31 15:52:52 +0200

    Sec9_Chap99-Adding Bean Validation related: changes in Application

--- example_24-pierrot/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ example_24-pierrot/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -4,10 +4,16 @@ import com.eazybytes.eazyschool.model.Contact;
 import com.eazybytes.eazyschool.service.ContactService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -21,12 +27,20 @@ public class ContactController {
     }
 
     @GetMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact",new Contact());
         return "contact.html";
     }
 
     @PostMapping("/saveMsg")
-    public ModelAndView saveMessage(Contact contact){
+    public ModelAndView saveMessage(@Valid Contact contact, Errors errors){
+
+        if (errors.hasErrors()){
+            log.error("Contact form Validation errors: {}",
+                    errors.getFieldErrors().stream().map((DefaultMessageSourceResolvable::getDefaultMessage))
+                            .collect(Collectors.toList()));
+            return new ModelAndView("contact.html");
+        }
         contactService.saveMessageDetails(contact);
         return new ModelAndView("redirect:/contact");
     }
