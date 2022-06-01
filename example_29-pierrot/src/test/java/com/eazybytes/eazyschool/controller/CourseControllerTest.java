c50435072fa01d618eb667e0e9c07b3e44be1363
PierreSQS <nomo_messina@web.de>
2022-06-01 11:32:29 +0200

    Sec13_Chap116-Configure multiples users: extended Controller Test (with UserDetails)

--- example_29-pierrot/src/test/java/com/eazybytes/eazyschool/controller/CourseControllerTest.java
+++ example_29-pierrot/src/test/java/com/eazybytes/eazyschool/controller/CourseControllerTest.java
@@ -4,6 +4,8 @@ import com.eazybytes.eazyschool.config.WebConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -31,8 +33,21 @@ class CourseControllerTest {
     }
 
     @Test
-    void displayCoursePageWithRealUser() throws Exception{
-        mockMvc.perform(get("/courses").with(user("eazybytes").password("12345")))
+    void displayCoursePageWithMockUser2() throws Exception{
+        mockMvc.perform(get("/courses").with(user("mock user").password("12345")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("courses"))
+                .andDo(print());
+    }
+
+    @Test
+    void displayCoursePageWithUserDetails() throws Exception{
+        UserDetails user = User.withUsername("Pierrot User Details")
+                .password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
+                .roles("USER")
+                .build();
+
+        mockMvc.perform(get("/courses").with(user(user)))
                 .andExpect(status().isOk())
                 .andExpect(view().name("courses"))
                 .andDo(print());
