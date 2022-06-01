8676c2531a66ff7b2e751055dd5d8d99a7266a98
PierreSQS <nomo_messina@web.de>
2022-06-02 01:02:04 +0200

    Sec16_Chap128-Setup H2 Database: init commit

---
+++ example_32-pierrot/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
@@ -0,0 +1,52 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Holiday;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+
+@Slf4j
+@Controller
+public class HolidaysController {
+
+    public static final String FESTIVAL_ATTR = "festival";
+    public static final String FEDERAL_ATTR = "federal";
+
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display,Model model) {
+        if(null != display && display.equals("all")){
+            model.addAttribute(FESTIVAL_ATTR,true);
+            model.addAttribute(FEDERAL_ATTR,true);
+        }else if(null != display && display.equals(FEDERAL_ATTR)){
+            model.addAttribute(FEDERAL_ATTR,true);
+        }else if(null != display && display.equals(FESTIVAL_ATTR)){
+            model.addAttribute(FESTIVAL_ATTR,true);
+        }
+        List<Holiday> holidays = Arrays.asList(
+                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
+                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
+                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
+                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
+                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
+                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
+        );
+
+        Map<Holiday.Type, List<Holiday>> holidaysMap = holidays.stream().collect(groupingBy(Holiday::getType));
+        holidaysMap.forEach((type, holidays1) -> {
+            log.info("Holiday Type: {}, Holidays: {}",type,holidays1);
+            model.addAttribute(type.toString(),holidays1);
+        });
+        return "holidays.html";
+    }
+
+}
