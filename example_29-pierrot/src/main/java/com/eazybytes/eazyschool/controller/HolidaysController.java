04f138dfbb185c14d45d37dde4f7eaba48d9eacb
PierreSQS <nomo_messina@web.de>
2022-06-01 10:26:24 +0200

    Sec13_Chap116-Configure multiples users: optimized Controller code

--- example_29-pierrot/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
+++ example_29-pierrot/src/main/java/com/eazybytes/eazyschool/controller/HolidaysController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -28,7 +29,7 @@ public class HolidaysController {
         }else if(null != display && display.equals(FESTIVAL_ATTR)){
             model.addAttribute(FESTIVAL_ATTR,true);
         }
-        List<Holiday> holidays = Arrays.asList(
+        List<Holiday> holidays = List.of(
                 new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
                 new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
                 new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
@@ -38,11 +39,12 @@ public class HolidaysController {
                 new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
                 new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
         );
-        Holiday.Type[] types = Holiday.Type.values();
-        for (Holiday.Type type : types) {
-            model.addAttribute(type.toString(),
-                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
-        }
+        Map<Holiday.Type, List<Holiday>> holidaysMap = holidays.stream().collect(Collectors.groupingBy(Holiday::getType));
+
+        holidaysMap.forEach((type, holidays1) -> {
+            log.info("#### Holiday Type: {}, Holidays: {}",type, holidays1);
+            model.addAttribute(type.toString(),holidays1);
+        });
         return "holidays.html";
     }
 
