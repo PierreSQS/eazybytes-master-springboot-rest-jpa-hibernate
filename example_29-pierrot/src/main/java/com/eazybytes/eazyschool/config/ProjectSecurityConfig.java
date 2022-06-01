886cdc6ecae33b310599e444856e82990be861af
PierreSQS <nomo_messina@web.de>
2022-06-01 11:31:42 +0200

    Sec13_Chap116-Configure multiples users: added InMemory Users

--- example_29-pierrot/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_29-pierrot/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.eazybytes.eazyschool.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,7 +13,7 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
             http.csrf().disable()
                 .authorizeRequests()
-                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/home").authenticated()
                 .mvcMatchers("/holidays/**").permitAll()
                 .mvcMatchers("/contact").permitAll()
                 .mvcMatchers("/saveMsg").permitAll()
@@ -22,4 +23,15 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                    .password("{bcrypt}$2a$10$F1GQjxZSwZjCbb.75hHsn.7cNLwNQsR48EnWqWSpZN2Ctu.MjLiW.")
+                    .roles("USER")
+                .and()
+                .withUser("admin").
+                    password("{bcrypt}$2a$10$YK339vcP40I6BJp0jQDCSewyFu7FGqNgkWwA5WD2y2.VpxCg1fry.")
+                    .roles("USER","ADMIN");
+    }
 }
