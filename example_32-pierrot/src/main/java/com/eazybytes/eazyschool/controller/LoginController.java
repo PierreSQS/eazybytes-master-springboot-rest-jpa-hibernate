438a730a078bed14c870ae3387d92497e5103249
PierreSQS <nomo_messina@web.de>
2022-06-02 19:44:41 +0200

    Sec16_Chap132-Saving Contact Msg: added a Controller Test and refined code

--- example_32-pierrot/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
+++ example_32-pierrot/src/main/java/com/eazybytes/eazyschool/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,17 +16,17 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value ="/login")
-    public String displayLoginPage(@RequestParam(value = "error", required = false) String error,
-                                   @RequestParam(value = "logout", required = false) String logout,Model model) {
-        String errorMessge = null;
+    @GetMapping(value ="/login")
+    public String displayLoginPage(@RequestParam(required = false) String error,
+                                   @RequestParam( required = false) String logout,Model model) {
+        String errorMessage = null;
         if(error != null) {
-            errorMessge = "Username or Password is incorrect !!";
+            errorMessage = "Username or Password is incorrect !!";
         }
         if(logout != null) {
-            errorMessge = "You have been successfully logged out !!";
+            errorMessage = "You have been successfully logged out !!";
         }
-        model.addAttribute("errorMessge", errorMessge);
+        model.addAttribute("errorMessage", errorMessage);
         return "login.html";
     }
 
