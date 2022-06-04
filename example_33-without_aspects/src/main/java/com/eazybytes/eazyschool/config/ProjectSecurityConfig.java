a7944b7b7ff38829f38e36e17d160f54fd0b0a15
PierreSQS <nomo_messina@web.de>
2022-06-04 11:07:08 +0200

    Sec16_Chap137-Display Holidays: initial commit

---
+++ example_33-without_aspects/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.eazybytes.eazyschool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().ignoringAntMatchers("/saveMsg").ignoringAntMatchers("/h2-console/**").and()
+                .authorizeRequests()
+                .mvcMatchers("/dashboard").authenticated()
+                .mvcMatchers("/displayMessages").hasRole("ADMIN")
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .mvcMatchers("/login").permitAll()
+                .and().formLogin().loginPage("/login")
+                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
+                .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
+                .and().authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .and().httpBasic();
+
+            http.headers().frameOptions().disable();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+                .password("{bcrypt}$2a$10$lqFNs8/aKyO5Gl5ihsw/EOx4LraKipTVsPAVLspMk2mKH98xz5b0q").roles("USER")
+            .and()
+            .withUser("admin")
+                .password("{bcrypt}$2a$10$tAva8.Lb/.K0rwxScyDH5e5MYIEJUQhhNTaRaIc4EattoJiihg.b6").roles("USER", "ADMIN");
+    }
+
+}
---
+++ example_33-without_aspects/src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -0,0 +1,51 @@
+package com.eazybytes.eazyschool.service;
+
+import com.eazybytes.eazyschool.constants.EazySchoolConstants;
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.repository.ContactRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
+@Slf4j
+@Service
+@ApplicationScope
+public class ContactService {
+
+    private final ContactRepository contactRepo;
+
+    public ContactService(ContactRepository contactRepo){
+        log.info("Contact Service Bean initialized");
+        this.contactRepo = contactRepo;
+    }
+
+    /*
+     * Save Contact Details into DB
+     * @param the contact to submit
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact){
+        contact.setStatus(EazySchoolConstants.OPEN);
+        contact.setCreatedAt(LocalDateTime.now());
+        contact.setCreatedBy(contact.getName());
+
+        int savedContactCnt = contactRepo.saveContactMsg(contact);
+        log.info("the contact saved: {}", contact);
+        return (savedContactCnt > 0);
+    }
+
+    public List<Contact> findContactMsgWithOpenStatus(String status) {
+        return contactRepo.findMessagesWithOpenStatus(status);
+    }
+
+    public int updateContactStatus(Integer id, String updatedBy) {
+        return contactRepo.updateContact(id, EazySchoolConstants.CLOSE, updatedBy);
+    }
+}
