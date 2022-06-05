2974d54f3696f6a05f60b96fa195c4563b7aea86
PierreSQS <nomo_messina@web.de>
2022-06-05 09:18:54 +0200

    Sec18_Chap147_149 JDBC to JPA Part4: corrected test

--- example_35-pierrot-h2/src/test/java/com/eazybytes/eazyschool/controller/ContactControllerTest.java
+++ example_35-pierrot-h2/src/test/java/com/eazybytes/eazyschool/controller/ContactControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -71,7 +72,7 @@ class ContactControllerTest {
                 .andExpect(view().name("redirect:/contact"))
                 .andDo(print());
 
-        verify(contactSrvMock).saveMessageDetails(contact);
+        verify(contactSrvMock).saveMessageDetails(any());
 
     }
 
