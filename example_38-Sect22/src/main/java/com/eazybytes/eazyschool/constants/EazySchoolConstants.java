c54d200ed8c48e953845482d7d8be97a820e2562
PierreSQS <nomo_messina@web.de>
2022-06-12 01:33:39 +0200

    Sec22_Chap166-Implement Spring Security-NOK: optimized Constants (Error while saving the new registered user)

--- example_38-Sect22/src/main/java/com/eazybytes/eazyschool/constants/EazySchoolConstants.java
+++ example_38-Sect22/src/main/java/com/eazybytes/eazyschool/constants/EazySchoolConstants.java
@@ -2,8 +2,8 @@ package com.eazybytes.eazyschool.constants;
 
 public interface EazySchoolConstants {
 
-    public static final String OPEN = "Open";
-    public static final String CLOSE = "Close";
-    public static final String STUDENT_ROLE = "STUDENT";
-    public static final String ADMIN_ROLE = "ADMIN";
+    String OPEN = "Open";
+    String CLOSE = "Close";
+    String STUDENT_ROLE = "STUDENT";
+    String ADMIN_ROLE = "ADMIN";
 }
