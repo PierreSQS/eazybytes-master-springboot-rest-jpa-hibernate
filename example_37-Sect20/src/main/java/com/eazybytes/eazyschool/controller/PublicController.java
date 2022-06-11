bc3a13b42ee7b693ed6e6e88aa99f62c1d6793ae
PierreSQS <nomo_messina@web.de>
2022-06-11 19:17:30 +0200

    Sec21_Chap164-Making oneToOne-NOK: reimplemented the controller (Error while saving the new registered User)

--- example_37-Sect20/src/main/java/com/eazybytes/eazyschool/controller/PublicController.java
+++ example_37-Sect20/src/main/java/com/eazybytes/eazyschool/controller/PublicController.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Person;
 import com.eazybytes.eazyschool.service.PersonService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,11 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
+@Slf4j
 @Controller
 @RequestMapping("/public")
 public class PublicController {
 
+    private static final String REG_PAGE="register.html";
+
     private final PersonService personSrv;
+
 
     public PublicController(PersonService personSrv) {
         this.personSrv = personSrv;
@@ -25,17 +30,22 @@ public class PublicController {
     @GetMapping("/register")
     public String displayRegistrationPage(Model model) {
         model.addAttribute("person", new Person());
-        return "register.html";
+        return REG_PAGE;
     }
 
     @PostMapping("/createUser")
     public String createUser(@Valid @ModelAttribute Person person, Errors errors) {
         if (errors.hasErrors()){
-            return "register.html";
+            return REG_PAGE;
         }
 
-        personSrv.saveUser(person);
+        Person savedPerson = personSrv.saveUser(person);
 
-        return "redirect:/login?register=true";
+        if (savedPerson != null && savedPerson.getPersonId() > 0) {
+            return "redirect:/login?register=true";
+        } else {
+           return REG_PAGE;
+        }
+
     }
 }
