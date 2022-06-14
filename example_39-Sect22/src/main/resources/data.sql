cc9f645b38901a0c994dfa7d2ff8d6299e7f7075
PierreSQS <nomo_messina@web.de>
2022-06-14 04:07:16 +0200

    Sec22_Chap172-Implementing Password Hashing Part2: adapted the script

--- example_39-Sect22/src/main/resources/data.sql
+++ example_39-Sect22/src/main/resources/data.sql
@@ -29,8 +29,8 @@ INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
   VALUES ('STUDENT',CURDATE(),'DBA');
 
 INSERT INTO `person` (`name`,`email`,`pwd`,`mobile_number`,`created_at`, `created_by`, `role_id`)
-  VALUES ('user','user@eazyschool.com','12345','2323232323',CURDATE(),'DBA',2);
+  VALUES ('user','user@eazyschool.com','$2a$10$90ABJ5EM9BMiewtCTxJJDu/fe/59XaFadcfLJ.YNJxp2e89IQf7Q2','2323232323',CURDATE(),'DBA',2);
 
 INSERT INTO `person` (`name`,`email`,`pwd`,`mobile_number`,`created_at`, `created_by`, `role_id`)
-  VALUES ('admin','admin@eazyschool.com','54321','1212121212',CURDATE(),'DBA',1);
+  VALUES ('admin','admin@eazyschool.com','$2a$10$Iak07jw1WmxNeLv5iWjS3uIkU4GVlk53Gca/61n44Dl1IruM/0dDu','1212121212',CURDATE(),'DBA',1);
 
