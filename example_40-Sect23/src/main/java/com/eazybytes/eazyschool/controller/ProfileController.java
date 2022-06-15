ed98cace0775b7f53b5b238ae62c1b980fdbb5bc
PierreSQS <nomo_messina@web.de>
2022-06-15 21:11:34 +0200

    Sec23_Chap176-Fetch Data from DB: final solution

--- example_40-Sect23/src/main/java/com/eazybytes/eazyschool/controller/ProfileController.java
+++ example_40-Sect23/src/main/java/com/eazybytes/eazyschool/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package com.eazybytes.eazyschool.controller;
 
+import com.eazybytes.eazyschool.model.Address;
 import com.eazybytes.eazyschool.model.Person;
 import com.eazybytes.eazyschool.model.Profile;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,15 @@ public class ProfileController {
         Person loggedUser = (Person) httpSession.getAttribute("loggedUser");
         Profile profile = new Profile();
         profile.setName(loggedUser.getName());
+        profile.setEmail(loggedUser.getEmail());
+        profile.setMobileNumber(loggedUser.getMobileNumber());
+        Address address = loggedUser.getAddress();
+        if (loggedUser.getPersonId() > 0 && address != null){
+            profile.setAddress1(address.getAddress1());
+            profile.setCity(address.getCity());
+            profile.setState(address.getState());
+            profile.setZipCode(address.getZipCode());
+        }
         model.addAttribute("profile",profile);
         return "profile.html";
     }
