06fd77d77e27be3200efb60983e8d1ff268ef6a6
PierreSQS <nomo_messina@web.de>
2022-06-15 21:12:57 +0200

    Sec23_Chap176-Fetch Data from DB: refined tests

--- example_40-Sect23/src/test/java/com/eazybytes/eazyschool/controller/DashboardControllerTest.java
+++ example_40-Sect23/src/test/java/com/eazybytes/eazyschool/controller/DashboardControllerTest.java
@@ -1,12 +1,18 @@
 package com.eazybytes.eazyschool.controller;
 
+import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.repository.PersonRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,12 +24,26 @@ class DashboardControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    PersonRepository personRepoMock;
+
+    Person personMock;
+
     @BeforeEach
     void setUp() {
+        personMock = new Person();
+        personMock.setPersonId(1);
+        personMock.setMobileNumber("1111111111");
     }
 
     @Test
     void displayDashboardForMockUserROLE_USER() throws Exception {
+        // Given
+        personMock.setName("Mock User");
+
+        given(personRepoMock.findByEmail(anyString())).willReturn(personMock);
+
+        // When and Then
         mockMvc.perform(get("/dashboard").with(user("Mock User")))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("username",equalTo("Mock User")))
@@ -35,6 +55,12 @@ class DashboardControllerTest {
     }
     @Test
     void displayDashboardForMockUserROLE_ADMIN() throws Exception {
+        // Given
+        personMock.setName("Mock Admin");
+
+        given(personRepoMock.findByEmail(anyString())).willReturn(personMock);
+
+        // When and Then
         mockMvc.perform(get("/dashboard").with(user("Mock Admin").roles("ADMIN")))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("username",equalTo("Mock Admin")))
--- example_40-Sect23/src/test/java/com/eazybytes/eazyschool/controller/ProfileControllerTest.java
+++ example_40-Sect23/src/test/java/com/eazybytes/eazyschool/controller/ProfileControllerTest.java
@@ -1,11 +1,16 @@
 package com.eazybytes.eazyschool.controller;
 
+import com.eazybytes.eazyschool.model.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -19,10 +24,20 @@ class ProfileControllerTest {
 
     @Test
     void displayProfilePage() throws Exception {
-        mockMvc.perform(get("/displayProfile").with(user("Mock User")))
+        Map<String, Object> stringPersonMap = new HashMap<>();
+        Person personMock = new Person();
+        personMock.setPersonId(1);
+        personMock.setName("Mock User");
+        personMock.setMobileNumber("1111111111");
+
+        stringPersonMap.put("loggedUser",personMock);
+
+        mockMvc.perform(get("/displayProfile").sessionAttrs(stringPersonMap).with(user("Mock User")))
                 .andExpect(status().isOk())
+                .andExpect(model().attributeDoesNotExist("errormsg"))
                 .andExpect(view().name("profile.html"))
                 .andExpect(content().string(containsString("My Profile")))
+                .andExpect(content().string(not((containsString("oops...")))))
                 .andDo(print());
     }
 }
