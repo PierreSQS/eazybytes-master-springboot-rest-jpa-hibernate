1b7bdfbfc5adf62444476236b0fda38243a0174d
PierreSQS <nomo_messina@web.de>
2022-06-15 21:10:00 +0200

    Sec23_Chap176-Fetch Data from DB: fixed the issue in previous commit (9ceef469)

--- example_40-Sect23/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
+++ example_40-Sect23/src/main/java/com/eazybytes/eazyschool/controller/DashboardController.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 
@@ -21,7 +21,7 @@ public class DashboardController {
     }
 
 
-    @GetMapping("/dashboard")
+    @RequestMapping("/dashboard")
     public String displayDashboard(Model model, Authentication authentication, HttpSession httpSession) {
 
         Person foundPersByEmail = personRepo.findByEmail(authentication.getName());
