e64967f00b03e337279eaf3c76ddfa0f573ff681
PierreSQS <nomo_messina@web.de>
2022-06-17 12:34:09 +0200

    Sec23_Chap177-Save Data into DB: final solution, fixed the issue of commit(38dafb1f)

--- example_40-Sect23/src/test/java/com/eazybytes/eazyschool/controller/ProfileControllerTest.java
+++ example_40-Sect23/src/test/java/com/eazybytes/eazyschool/controller/ProfileControllerTest.java
@@ -1,6 +1,9 @@
 package com.eazybytes.eazyschool.controller;
 
+import com.eazybytes.eazyschool.constants.EazySchoolConstants;
+import com.eazybytes.eazyschool.model.Address;
 import com.eazybytes.eazyschool.model.Person;
+import com.eazybytes.eazyschool.model.Roles;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +27,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(ProfileController.class)
 class ProfileControllerTest {
 
+    // Person to update
+    private Person personToUpdate;
+
+    // Session Map for the Request
     private Map<String,Object> stringPersonMap = new HashMap<>();
 
     @Autowired
@@ -31,12 +38,37 @@ class ProfileControllerTest {
 
     @BeforeEach
     void setUp() {
+
+        // Role to set for a Person
+        Roles roles = new Roles();
+        roles.setRoleName(EazySchoolConstants.STUDENT_ROLE);
+
+        // Address to set for a Person
+        Address address = new Address();
+        address.setAddress1("Rue des Manguiers");
+        address.setCity("Douala");
+        address.setState("Wouri");
+        address.setZipCode("12345");
+
+        personToUpdate = new Person();
+        personToUpdate.setPersonId(1);
+        personToUpdate.setName("Pierrot");
+        personToUpdate.setEmail("pierrot@gmail.com");
+        personToUpdate.setConfirmEmail("pierrot@gmail.com");
+        personToUpdate.setMobileNumber("1234567890");
+        personToUpdate.setPwd("galerien");
+        personToUpdate.setConfirmPwd("galerien");
+        personToUpdate.setRoles(roles);
+        personToUpdate.setAddress(address);
+
         stringPersonMap = new HashMap<>();
+
         Person personMock = new Person();
         personMock.setPersonId(1);
         personMock.setName("Mock User");
         personMock.setMobileNumber("1111111111");
 
+        // Sets the Session Attribute for the Request
         stringPersonMap.put("loggedUser",personMock);
     }
     @Test
@@ -55,6 +87,16 @@ class ProfileControllerTest {
     @Test
     void updateProfilePage() throws Exception {
         MultiValueMap<String,String> multiValueMap = new LinkedMultiValueMap<>();
+        multiValueMap.add("name", personToUpdate.getName());
+        multiValueMap.add("mobileNumber", personToUpdate.getMobileNumber());
+        multiValueMap.add("email", personToUpdate.getEmail());
+        multiValueMap.add("confirmEmail", personToUpdate.getConfirmEmail());
+        multiValueMap.add("pwd", personToUpdate.getPwd());
+        multiValueMap.add("confirmPwd", personToUpdate.getConfirmPwd());
+        multiValueMap.add("address1",personToUpdate.getAddress().getAddress1());
+        multiValueMap.add("city",personToUpdate.getAddress().getCity());
+        multiValueMap.add("state",personToUpdate.getAddress().getState());
+        multiValueMap.add("zipCode",personToUpdate.getAddress().getZipCode());
 
         mockMvc.perform(post("/updateProfile").sessionAttrs(stringPersonMap)
                         .with(csrf())
