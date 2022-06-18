33b9809a2bf587ee0298104cde37682a6c806b79
PierreSQS <nomo_messina@web.de>
2022-06-18 13:23:26 +0200

    Sec24_Chap179-Displaying Classes: Pierrot initial Code

---
+++ example_41-Sect24/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -0,0 +1,59 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.service.ContactService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Slf4j
+@Controller
+public class ContactController {
+
+    private final ContactService contactService;
+
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @RequestMapping("/contact")
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
+        return "contact.html";
+    }
+
+    @PostMapping("/saveMsg")
+    public String saveMessage(@Valid @ModelAttribute Contact contact, Errors errors) {
+        if(errors.hasErrors()){
+            log.error("Contact form validation failed due to : " + errors.getFieldErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage).toList());
+            return "contact.html";
+        }
+        contactService.saveMessageDetails(contact);
+        return "redirect:/contact";
+    }
+
+    @GetMapping("/displayMessages")
+    public ModelAndView displayMessages(Model model) {
+        List<Contact> contactMsgs = contactService.findMsgsWithOpenStatus();
+        ModelAndView modelAndView = new ModelAndView("messages.html");
+        modelAndView.addObject("contactMsgs",contactMsgs);
+        return modelAndView;
+    }
+
+    @GetMapping("/closeMsg")
+    public String closeMsg(@RequestParam int id) {
+        contactService.updateContactStatus(id);
+        return "redirect:/displayMessages";
+    }
+
+
+
+}
