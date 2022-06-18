fe42971d98815ab5bab1cacaf302bfb0da4cccfa
PierreSQS <nomo_messina@web.de>
2022-06-18 11:30:24 +0200

    Sec23_Chap177-Save Data into DB-NOK: finalizing the Controller

--- example_40-Sect23/src/main/java/com/eazybytes/eazyschool/controller/ProfileController.java
+++ example_40-Sect23/src/main/java/com/eazybytes/eazyschool/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.eazybytes.eazyschool.controller;
 import com.eazybytes.eazyschool.model.Address;
 import com.eazybytes.eazyschool.model.Person;
 import com.eazybytes.eazyschool.model.Profile;
+import com.eazybytes.eazyschool.repository.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,9 +17,16 @@ import javax.validation.Valid;
 @Controller
 public class ProfileController {
 
+    public static final String LOGGED_USER = "loggedUser";
+    private final PersonRepository personRepo;
+
+    public ProfileController(PersonRepository personRepo) {
+        this.personRepo = personRepo;
+    }
+
     @GetMapping("/displayProfile")
     public String displayProfilePage(Model model, HttpSession httpSession) {
-        Person loggedUser = (Person) httpSession.getAttribute("loggedUser");
+        Person loggedUser = (Person) httpSession.getAttribute(LOGGED_USER);
         Profile profile = new Profile();
         profile.setName(loggedUser.getName());
         profile.setEmail(loggedUser.getEmail());
@@ -39,6 +47,32 @@ public class ProfileController {
         if (errors.hasErrors()) {
             return "profile.html";
         }
+
+        Person loggedUser = (Person) httpSession.getAttribute(LOGGED_USER);
+        loggedUser.setName(profile.getName());
+        loggedUser.setEmail(profile.getEmail());
+        loggedUser.setMobileNumber(profile.getMobileNumber());
+        Address address = loggedUser.getAddress();
+        if (address == null) {
+            address = new Address();
+        }
+
+        // update the Address
+        address.setAddress1(profile.getAddress1());
+        address.setAddress2(profile.getAddress2());
+        address.setCity(profile.getCity());
+        address.setState(profile.getState());
+        address.setZipCode(profile.getZipCode());
+
+        // reassign the updated address
+        loggedUser.setAddress(address);
+
+        // update the loggedInUser's Address
+        personRepo.save(loggedUser);
+
+        // set the session Attribute to keep the data on Form
+        httpSession.setAttribute(LOGGED_USER,loggedUser);
+
         return "redirect:/displayProfile";
     }
 
