a04dbb36a15e6d048d664ddd7bbdade70344dc56
PierreSQS <nomo_messina@web.de>
2022-06-19 16:53:51 +0200

    Sec24_Chap183-Add Classes enhancement: extended and enhanced the Test

--- example_41-Sect24/src/test/java/com/eazybytes/eazyschool/controller/AdminControllerTest.java
+++ example_41-Sect24/src/test/java/com/eazybytes/eazyschool/controller/AdminControllerTest.java
@@ -1,25 +1,54 @@
 package com.eazybytes.eazyschool.controller;
 
+import com.eazybytes.eazyschool.model.EazyClass;
+import com.eazybytes.eazyschool.repository.EazyClassRepository;
+import com.eazybytes.eazyschool.repository.PersonRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = {AdminController.class})
 class AdminControllerTest {
 
+    List<EazyClass> eazyClassesMock;
+
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    EazyClassRepository eazyClassRepoMock;
+
+    @MockBean
+    PersonRepository personRepoMock;
+
     @BeforeEach
     void setUp() {
+        EazyClass class1 = new EazyClass();
+        class1.setClassId(1);
+        class1.setName("Music");
+
+        EazyClass class2 = new EazyClass();
+        class1.setClassId(2);
+        class2.setName("Java");
+
+        eazyClassesMock = List.of(class1,class2);
+        given(eazyClassRepoMock.findAll()).willReturn(eazyClassesMock);
     }
 
     @Test
@@ -30,6 +59,23 @@ class AdminControllerTest {
                 .andExpect(model().attributeExists("eazyClass"))
                 .andExpect(view().name("classes.html"))
                 .andExpect(content().string(containsString("EazySchool Class Details")))
+                .andExpect(content().string(containsString("Music")))
+                .andExpect(content().string(containsString("Java")))
+                .andDo(print());
+    }
+
+    @Test
+    @WithMockUser(username = "Mock Admin",roles = {"ADMIN"})
+    void createClass() throws Exception {
+        EazyClass aClass = new EazyClass();
+        aClass.setName("A Class");
+
+        MultiValueMap<String,String> multiValueMap = new LinkedMultiValueMap<>();
+        multiValueMap.add("name",aClass.getName());
+
+        mockMvc.perform(post("/admin/addNewClass").with(csrf()).params(multiValueMap))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/admin/displayClasses"))
                 .andDo(print());
     }
 }
