29a4df3c7e1aa30b0eb15c0e88d8feb8735ec2dc
PierreSQS <nomo_messina@web.de>
2022-06-26 02:50:24 +0200

    Sec24_Chap184-Display Students: related code

--- example_41-Sect24/src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
+++ example_41-Sect24/src/main/java/com/eazybytes/eazyschool/controller/AdminController.java
@@ -1,12 +1,14 @@
 package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.EazyClass;
+import com.eazybytes.eazyschool.model.Person;
 import com.eazybytes.eazyschool.repository.EazyClassRepository;
 import com.eazybytes.eazyschool.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
@@ -54,4 +56,14 @@ public class AdminController {
 
         return "redirect:/admin/displayClasses";
     }
+
+    @GetMapping("displayStudents")
+    public ModelAndView displayStudents(@RequestParam int classId) {
+        ModelAndView modelAndView = new ModelAndView("Students.html");
+        Optional<EazyClass> classByIDOpt = eazyClassRepo.findById(classId);
+        classByIDOpt.ifPresent(eazyClass -> modelAndView.addObject("eazyClass", eazyClass));
+        modelAndView.addObject("person",new Person());
+        return modelAndView;
+    }
+
 }
