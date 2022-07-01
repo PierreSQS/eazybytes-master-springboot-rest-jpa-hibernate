1728b79b646d044fb7537497f25dd29ad3dd0169
PierreSQS <nomo_messina@web.de>
2022-07-01 16:06:43 +0200

    Sec25_Chap187-Implement ManyToMany: optimized Entity

--- example_42-Sect25/src/main/java/com/eazybytes/eazyschool/model/Courses.java
+++ example_42-Sect25/src/main/java/com/eazybytes/eazyschool/model/Courses.java
@@ -1,12 +1,17 @@
 package com.eazybytes.eazyschool.model;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 public class Courses extends BaseEntity{
     @Id
@@ -18,6 +23,19 @@ public class Courses extends BaseEntity{
     private String fees;
 
     @ManyToMany(mappedBy = "courses", fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @ToString.Exclude
     Set<Person> person = new HashSet<>(); // This initialization can be helpfully for the Tests!!!
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Courses courses = (Courses) o;
+        return courseId != null && Objects.equals(courseId, courses.courseId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
