a81d9c648f5759f4e272124eba2ba69c6403ded0
PierreSQS <nomo_messina@web.de>
2022-07-07 23:26:29 +0200

    Sec27_Chap200-Custom Queries: implemented custom query

--- example_44-Sect27/src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
+++ example_44-Sect27/src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
@@ -4,6 +4,8 @@ import com.eazybytes.eazyschool.model.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /*
@@ -14,6 +16,7 @@ DB related operations and
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
-    Page<Contact> findByStatus(String status, Pageable pageable);
-
+    //@Query("SELECT c FROM Contact c WHERE c.status = :status")
+    @Query(value = "SELECT * FROM contact_msg c WHERE c.status = :status",nativeQuery = true)
+    Page<Contact> findByStatus(@Param("status") String status, Pageable pageable); //@Param not necessary
 }
