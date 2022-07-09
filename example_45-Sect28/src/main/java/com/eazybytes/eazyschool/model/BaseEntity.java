9d9b2f82ebeaf10253967ba0428f60feb7eda887
PierreSQS <nomo_messina@web.de>
2022-07-10 01:31:43 +0200

    Sec28_Chap216-Content Filtering: adding property filtering

--- example_45-Sect28/src/main/java/com/eazybytes/eazyschool/model/BaseEntity.java
+++ example_45-Sect28/src/main/java/com/eazybytes/eazyschool/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.eazybytes.eazyschool.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
@@ -22,14 +23,21 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
+    @JsonIgnore
     private LocalDateTime createdAt;
+
     @CreatedBy
     @Column(updatable = false)
+    @JsonIgnore
     private String createdBy;
+
     @LastModifiedDate
     @Column(insertable = false)
+    @JsonIgnore
     private LocalDateTime updatedAt;
+
     @LastModifiedBy
     @Column(insertable = false)
+    @JsonIgnore
     private String updatedBy;
 }
