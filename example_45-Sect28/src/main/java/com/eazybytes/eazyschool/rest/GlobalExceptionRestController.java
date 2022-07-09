3315a20a563c1fe17ca67fc449c0248e43802770
PierreSQS <nomo_messina@web.de>
2022-07-09 11:58:58 +0200

    Sec28_Chap213-Implement Error Logic: initial GlobalExceptionRestController

---
+++ example_45-Sect28/src/main/java/com/eazybytes/eazyschool/rest/GlobalExceptionRestController.java
@@ -0,0 +1,21 @@
+package com.eazybytes.eazyschool.rest;
+
+import com.eazybytes.eazyschool.model.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(annotations = {RestController.class})
+public class GlobalExceptionRestController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> exceptionHandler(Exception exception) {
+        Response errorResponse = new Response(HttpStatus.INTERNAL_SERVER_ERROR.toString(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
