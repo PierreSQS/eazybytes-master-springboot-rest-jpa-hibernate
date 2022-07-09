34197b857706528edb8634e8952a3f4533fba32b
PierreSQS <nomo_messina@web.de>
2022-07-09 15:03:03 +0200

    Sec28_Chap213-Implement Error Logic: introduced invalid Param Exception Handler

--- example_45-Sect28/src/main/java/com/eazybytes/eazyschool/rest/GlobalExceptionRestController.java
+++ example_45-Sect28/src/main/java/com/eazybytes/eazyschool/rest/GlobalExceptionRestController.java
@@ -2,17 +2,46 @@ package com.eazybytes.eazyschool.rest;
 
 import com.eazybytes.eazyschool.model.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice(annotations = {RestController.class})
 @Order(1)
-public class GlobalExceptionRestController {
+public class GlobalExceptionRestController extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        Response errorResponse = new Response();
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        String errorMsgs = fieldErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(" ; "));
+
+        log.info("####### the error Messages: {} #########",errorMsgs);
+
+        errorResponse.setStatusCode(status.toString());
+        errorResponse.setStatusMsg(errorMsgs);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Response> exceptionHandler(Exception exception) {
