bb6b9cbedd997111d7ab7fdec0d5601c2a624af5
PierreSQS <nomo_messina@web.de>
2022-07-10 17:04:59 +0200

    Sec29_Chap219-Consuming REST OpenFeign: added a test

---
+++ example_46-Sect29/src/test/java/com/eazybytes/eazyschool/controller/ContactControllerTest.java
@@ -0,0 +1,64 @@
+package com.eazybytes.eazyschool.controller;
+
+import com.eazybytes.eazyschool.constants.WebClientConstants;
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.proxy.ContactProxy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ContactController.class)
+class ContactControllerTest {
+
+    private List<Contact> contactMessagesMock;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ContactProxy contactProxyMock;
+
+    @BeforeEach
+    void setUp() {
+        Contact contact1 = new Contact();
+        contact1.setContactId(1);
+        contact1.setName("Sarah Test");
+        contact1.setEmail("sarah@example.com");
+        contact1.setMessage("Please contact me");
+        contact1.setSubject("concerning my music course");
+        contact1.setStatus(WebClientConstants.OPEN);
+
+        Contact contact2 = new Contact();
+        contact2.setContactId(2);
+        contact2.setName("Jeannot Test");
+        contact2.setEmail("jeannot@example.com");
+        contact2.setMessage("A Question");
+        contact2.setSubject("is there any Music course?");
+        contact1.setStatus(WebClientConstants.OPEN);
+
+        contactMessagesMock = List.of(contact1,contact2);   }
+
+    @Test
+    void getMessages() throws Exception {
+        given(contactProxyMock.getMessagesByStatus(WebClientConstants.OPEN)).willReturn(contactMessagesMock);
+
+        mockMvc.perform(get("/getMessages")
+                        .param("status","Open"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.size()").value(equalTo(2)))
+                .andDo(print());
+    }
+}
