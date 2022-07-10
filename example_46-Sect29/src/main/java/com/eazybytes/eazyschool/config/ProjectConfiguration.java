92441c2f21bb943ff86bd967a1a43da0f60416c8
PierreSQS <nomo_messina@web.de>
2022-07-11 00:05:04 +0200

    Sec29_Chap220-Consuming REST RestTemplate: related Code

--- example_46-Sect29/src/main/java/com/eazybytes/eazyschool/config/ProjectConfiguration.java
+++ example_46-Sect29/src/main/java/com/eazybytes/eazyschool/config/ProjectConfiguration.java
@@ -1,8 +1,10 @@
 package com.eazybytes.eazyschool.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ProjectConfiguration {
@@ -13,6 +15,14 @@ public class ProjectConfiguration {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
         return new BasicAuthRequestInterceptor(ADMIN_EAZYSCHOOL, ADMIN_PWD);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder
+                .basicAuthentication(ADMIN_EAZYSCHOOL,ADMIN_PWD)
+                .build();
     }
 
 }
--- example_46-Sect29/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ example_46-Sect29/src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -1,10 +1,14 @@
 package com.eazybytes.eazyschool.controller;
 
 import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.model.Response;
 import com.eazybytes.eazyschool.proxy.ContactProxy;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -12,14 +16,26 @@ import java.util.List;
 public class ContactController {
 
     private final ContactProxy contactProxy;
+    private final RestTemplate restTemplate;
 
-    public ContactController(ContactProxy contactProxy) {
+    public ContactController(ContactProxy contactProxy, RestTemplate restTemplate) {
         this.contactProxy = contactProxy;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/getMessages")
     public List<Contact> getMessages(@RequestParam String status) {
         return contactProxy.getMessagesByStatus(status);
+    }
+
+    @PostMapping("saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestBody Contact contact) {
+        String url = "http://localhost:8080/api/contact/saveMsg";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("invocationFrom","RestTemplate");
+
+        HttpEntity<Contact> httpEntity = new HttpEntity<>(contact, httpHeaders);
+        return restTemplate.exchange(url, HttpMethod.POST,httpEntity,Response.class);
     }
 
 }
