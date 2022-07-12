e98198e25e1e731b19c6c2d7a26c0e09f4e78183
PierreSQS <nomo_messina@web.de>
2022-07-12 15:59:41 +0200

    Sec30_Chap223-Spring Data Rest-NOK: trying to debug the issue in the previous commit(76565385)

--- example_47-Sect30/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
+++ example_47-Sect30/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
@@ -1,7 +1,12 @@
 package com.eazybytes.eazyschool;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EazyschoolApplication {
@@ -9,5 +14,22 @@ public class EazyschoolApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EazyschoolApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("########## Let's inspect the beans provided by Spring Boot: " +
+					"########## Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+
+			Arrays.stream(beanNames)
+					.filter(beanName -> beanName.contains("rofileController"))
+					.forEach(System.out::println);
+
+		};
+	}
+
 
 }
