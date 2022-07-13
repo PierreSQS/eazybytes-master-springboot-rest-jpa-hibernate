54562174f16af73bf747c71916be481a1d737a6a
PierreSQS <nomo_messina@web.de>
2022-07-13 11:48:22 +0200

    Sec30_Chap226-Quick Tips on HAL: hidden a JPA repository to HAL Explorer

--- example_47-Sect30/src/main/java/com/eazybytes/eazyschool/repository/RolesRepository.java
+++ example_47-Sect30/src/main/java/com/eazybytes/eazyschool/repository/RolesRepository.java
@@ -2,11 +2,14 @@ package com.eazybytes.eazyschool.repository;
 
 import com.eazybytes.eazyschool.model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+//@RepositoryRestResource(path = "roles") just for documentation
+@RepositoryRestResource(exported = false)
 public interface RolesRepository extends JpaRepository<Roles, Integer> {
 
     List<Roles> getByRoleName(String roleName);
