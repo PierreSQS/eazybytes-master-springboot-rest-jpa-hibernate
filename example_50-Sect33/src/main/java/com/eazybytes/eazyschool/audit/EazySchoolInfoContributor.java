033c1f4753f530cfe1c2b07a9d5733b61df7b689
PierreSQS <nomo_messina@web.de>
2022-07-31 22:47:24 +0200

    Sec33_Chap243-Deep Dive of Actuator: related changes

---
+++ example_50-Sect33/src/main/java/com/eazybytes/eazyschool/audit/EazySchoolInfoContributor.java
@@ -0,0 +1,20 @@
+package com.eazybytes.eazyschool.audit;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class EazySchoolInfoContributor implements InfoContributor {
+    @Override
+    public void contribute(Info.Builder builder) {
+        Map<String, String> eazyMap = new HashMap<>();
+        eazyMap.put("App Name", "EazySchool");
+        eazyMap.put("App Description", "Eazy Web Application for Students and Admin");
+        eazyMap.put("App Version", "1.0.0");
+        builder.withDetail("eazyschool-info",eazyMap);
+    }
+}
