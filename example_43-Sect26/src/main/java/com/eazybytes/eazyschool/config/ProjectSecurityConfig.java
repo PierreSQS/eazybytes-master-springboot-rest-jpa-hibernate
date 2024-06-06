d2a48b0c8423303e50e5deea0b0726ad3cb684b2
PierreSQS <nomo_messina@web.de>
2024-06-07 00:16:30 +0200

    Sec25_Chap198-XXX: Pierrot Security Config

--- example_43-Sect26/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ example_43-Sect26/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -17,32 +17,65 @@ public class ProjectSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/saveMsg")
-                        .ignoringRequestMatchers("/public/**"))
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/dashboard").authenticated()
-                        .requestMatchers("/displayProfile").authenticated()
-                        .requestMatchers("/updateProfile").authenticated()
-                        .requestMatchers("/displayMessages/**").hasRole(ADMIN_ROLE)
-                        .requestMatchers("/closeMsg/**").hasRole(ADMIN_ROLE)
-                        .requestMatchers("/admin/**").hasRole(ADMIN_ROLE)
-                        .requestMatchers("/student/**").hasRole(STUDENT_ROLE)
-                        .requestMatchers("/", "/home").permitAll()
-                        .requestMatchers("/holidays/**").permitAll()
-                        .requestMatchers("/contact").permitAll()
-                        .requestMatchers("/saveMsg").permitAll()
-                        .requestMatchers("/courses").permitAll()
-                        .requestMatchers("/about").permitAll()
-                        .requestMatchers("/assets/**").permitAll()
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("/logout").permitAll()
-                        .requestMatchers("/public/**").permitAll())
-                .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
-                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
-                .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+
+        http.csrf(csrfConfigurer -> csrfConfigurer
+                        .ignoringRequestMatchers("/saveMsg", "/public/**"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/dashboard",
+                                "/displayProfile",
+                                "/updateProfile").authenticated()
+                        .requestMatchers(
+                                "/displayMessages/**",
+                                "/admin/**").hasRole(ADMIN_ROLE)
+                        .requestMatchers(
+                                "/student/**").hasRole(STUDENT_ROLE).
+                        requestMatchers(
+                                "/home",
+                                "/holidays/**",
+                                "/contact",
+                                "/saveMsg",
+                                "/courses",
+                                "/about",
+                                "/login",
+                                "/logout",
+                                "/public/**",
+                                "/assets/**").permitAll())
+                .formLogin(loginConfigurer -> loginConfigurer
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard")
+                        .failureUrl("/login?error=true").permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer
+                        .logoutSuccessUrl("/login?logout=true")
                         .invalidateHttpSession(true).permitAll())
                 .httpBasic(Customizer.withDefaults());
+
+//        http.csrf(csrf -> csrf
+//                        .ignoringRequestMatchers("/saveMsg")
+//                        .ignoringRequestMatchers("/public/**"))
+//                .authorizeHttpRequests(requests -> requests
+//                        .requestMatchers("/dashboard").authenticated()
+//                        .requestMatchers("/displayProfile").authenticated()
+//                        .requestMatchers("/updateProfile").authenticated()
+//                        .requestMatchers("/displayMessages/**").hasRole(ADMIN_ROLE)
+//                        .requestMatchers("/closeMsg/**").hasRole(ADMIN_ROLE)
+//                        .requestMatchers("/admin/**").hasRole(ADMIN_ROLE)
+//                        .requestMatchers("/student/**").hasRole(STUDENT_ROLE)
+//                        .requestMatchers("/", "/home").permitAll()
+//                        .requestMatchers("/holidays/**").permitAll()
+//                        .requestMatchers("/contact").permitAll()
+//                        .requestMatchers("/saveMsg").permitAll()
+//                        .requestMatchers("/courses").permitAll()
+//                        .requestMatchers("/about").permitAll()
+//                        .requestMatchers("/assets/**").permitAll()
+//                        .requestMatchers("/login").permitAll()
+//                        .requestMatchers("/logout").permitAll()
+//                        .requestMatchers("/public/**").permitAll())
+//                .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+//                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+//                .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+//                        .invalidateHttpSession(true).permitAll())
+//                .httpBasic(Customizer.withDefaults());
 
             return http.build();
     }
