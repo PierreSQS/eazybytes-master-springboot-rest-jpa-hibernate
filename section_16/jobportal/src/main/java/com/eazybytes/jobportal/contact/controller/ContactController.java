f475dd138264ae657d16c4c05b7ef3e20f76e6b0
PierreSQS <nomo_messina@web.de>
2026-03-14 10:40:57 +0100

    Sec16_Chap174: fixed the issue (2882b51b) when closing a contact message

--- section_16/jobportal/src/main/java/com/eazybytes/jobportal/contact/controller/ContactController.java
+++ section_16/jobportal/src/main/java/com/eazybytes/jobportal/contact/controller/ContactController.java
@@ -62,12 +62,12 @@ public class ContactController {
     public ResponseEntity<String> closeContactMsg(@PathVariable Long id) {
         boolean isClosed = contactService.closeContactMsg(id);
         if (isClosed) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("the contact path is wrong since the id doesn't exist");
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("Contact Message successfully closed");
         }
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body("Message successfully closed");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("the contact path is wrong since the id doesn't exist");
     }
 
 }
